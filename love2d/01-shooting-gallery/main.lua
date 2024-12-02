8d5e637c9b149b89a76dd19f7911d845142a3cd6
m.bychkov <mikhail.bychkov@octo.uz>
2024-12-02 17:00:08 +0500

    target table

--- love2d/01-shooting-gallery/main.lua
+++ love2d/01-shooting-gallery/main.lua
@@ -1,12 +1,14 @@
 function love.load()
+    target = {}
+    target.x = 300
+    target.y = 300
+    target.radius = 50
 end
 
 function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.setColor(0, 0, 1)
-    love.graphics.rectangle("fill", 150, 200, 200, 100)
-    love.graphics.setColor(1, 0.4, 0)
-    love.graphics.circle("fill", 150, 150, 100)
+    love.graphics.setColor(1, 0, 0)
+    love.graphics.circle("fill", target.x, target.y, target.radius)
 end
