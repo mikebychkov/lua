41a8f23b366e4a836d87bba62f04258d7bd397e1
m.bychkov <mikhail.bychkov@octo.uz>
2024-12-02 15:27:15 +0500

    rectangle

--- love2d/01-shooting-gallery/main.lua
+++ love2d/01-shooting-gallery/main.lua
@@ -1,11 +1,9 @@
 function love.load()
-    number = 0
 end
 
 function love.update(dt)
-    number = number + 1
 end
 
 function love.draw()
-    love.graphics.print(number)
+    love.graphics.rectangle("fill", 100, 100, 200, 100)
 end
