81a74c90c9e3eac326dc55edfe7766ad4638b22b
m.bychkov <mikhail.bychkov@octo.uz>
2024-12-02 16:06:11 +0500

    drawing

--- love2d/01-shooting-gallery/main.lua
+++ love2d/01-shooting-gallery/main.lua
@@ -5,5 +5,8 @@ function love.update(dt)
 end
 
 function love.draw()
-    love.graphics.rectangle("fill", 100, 100, 200, 100)
+    love.graphics.setColor(0, 0, 1)
+    love.graphics.rectangle("fill", 150, 200, 200, 100)
+    love.graphics.setColor(1, 0.4, 0)
+    love.graphics.circle("fill", 150, 150, 100)
 end
