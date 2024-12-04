c7d670277e7b586632a9924e066737049c5b5ba8
m.bychkov <mikhail.bychkov@octo.uz>
2024-12-04 09:21:08 +0500

    init top-down-shooter 2

---
+++ love2d/03-top-down-shooter/main.lua
@@ -0,0 +1,25 @@
+
+-- EVENTS
+
+function love.load()
+    
+    sprites = {}
+    sprites.background = love.graphics.newImage('img/background.png')
+    sprites.bullet = love.graphics.newImage('img/bullet.png')
+    sprites.player = love.graphics.newImage('img/player.png')
+    sprites.zombie = love.graphics.newImage('img/zombie.png')
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+    
+end
+
+function love.mousepressed(x, y, button, istouch, presses)
+    
+end
+
+-- FUNCTIONS
