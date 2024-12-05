5e2110367ed8d116bd33d0b5810437b041a9a158
m.bychkov <mikhail.bychkov@octo.uz>
2024-12-05 18:39:26 +0500

    bullets my implementation

--- love2d/02-top-down-shooter/main.lua
+++ love2d/02-top-down-shooter/main.lua
@@ -3,6 +3,7 @@
 -- EVENTS -----------------------------------------------------------
 
 function love.load()
+
     sprites = {}
     sprites.background = love.graphics.newImage('img/background.png')
     sprites.bullet = love.graphics.newImage('img/bullet.png')
@@ -19,22 +20,38 @@ function love.load()
     player.r = 0
 
     zombies = {}
+
+    bullets = {}
 end
 
 function love.update(dt)
+
     if gameState == 0 then
         return
     end
     timer = timer + dt
     movePlayer(dt)
-    for i,z in ipairs(zombies) do
+    for i,b in ipairs(bullets) do
+        moveBullet(b, dt)
+    end
+    for zi,z in ipairs(zombies) do
         moveZombie(z, dt)
+        if distanceBetweenCoordinates(z.x, z.y, player.x, player.y) < 30 then
+            gameOver()
+        end
+        for bi,b in ipairs(bullets) do
+            if distanceBetweenCoordinates(z.x, z.y, b.x, b.y) < 30 then
+                zombies[zi] = nil
+                bullets[bi] = nil
+                score = score + 1
+            end
+        end
     end
 end
 
 function love.draw()
-    love.graphics.setColor(1,1,1)
-    love.graphics.draw(sprites.background, 0, 0)
+
+    drawBackground()
     if gameState > 0 then
         drawPlayer()
     else
@@ -44,14 +61,21 @@ function love.draw()
         for i,z in ipairs(zombies) do
             drawZombie(z)
         end
+        for i,b in ipairs(bullets) do
+            drawBullet(b)
+        end
     end
+    drawStats()
 end
 
 function love.mousepressed(x, y, button, istouch, presses)
+
     if gameState == 0 then
         gameState = 1
         return
     end
+
+    shootTheBullet()
 end
 
 function love.keypressed(key, scancode, isrepeat)
@@ -63,7 +87,15 @@ end
 
 -- FUNCTIONS --------------------------------------------------------
 
+function gameOver()
+    gameState = 0
+    for i,z in ipairs(zombies) do
+        zombies[i] = nil
+    end
+end
+
 function movePlayer(dt)
+
     if love.keyboard.isDown("w") then
         player.y = player.y - player.speed * dt
     end
@@ -85,9 +117,16 @@ function movePlayer(dt)
 end
 
 function moveZombie(z, dt)
+
     z.r = math.atan2(player.y - z.y, player.x - z.x)
     z.x = z.x + math.cos(z.r) * z.speed * dt
     z.y = z.y + math.sin(z.r) * z.speed * dt
+end
+
+function moveBullet(b, dt)
+
+    b.x = b.x + math.cos(b.r) * b.speed * dt
+    b.y = b.y + math.sin(b.r) * b.speed * dt
 end
 
 function reset()
@@ -95,7 +134,22 @@ function reset()
     timer = 0
 end
 
+function drawBackground()
+
+    love.graphics.setColor(1,1,1)
+    love.graphics.draw(sprites.background, 0, 0)
+end
+
+function drawStats()
+
+    love.graphics.setColor(0,0,0)
+    love.graphics.setFont(love.graphics.newFont(20))
+    love.graphics.print("Score: " .. score, 10, 10)
+    love.graphics.printf("Time: " .. math.ceil(timer), 0, 10, love.graphics.getWidth() - 10, "right")
+end
+
 function drawStartPrompt()
+
     love.graphics.setColor(1,0,0)
     love.graphics.setFont(love.graphics.newFont(50))
     love.graphics.printf("CLICK TO START", 0, love.graphics.getHeight() / 2, love.graphics.getWidth(), "center")
@@ -122,6 +176,14 @@ function drawZombie(z)
     local hh = sprite:getHeight() / 2
     local hw = sprite:getWidth() / 2
     love.graphics.draw(sprite, z.x, z.y, z.r, 1, 1, hw, hh)
+end
+
+function drawBullet(b)
+
+    local sprite = sprites.bullet
+    local hh = sprite:getHeight() / 2
+    local hw = sprite:getWidth() / 2
+    love.graphics.draw(sprite, b.x, b.y, b.r, 1, 1, hw, hh)
 end
 
 function drawSprite(sprite, x, y)
@@ -158,4 +220,20 @@ function spawnZombie()
     end
 
     table.insert(zombies, zombie)
+end
+
+function shootTheBullet()
+
+    bullet = {}
+    bullet.x = player.x
+    bullet.y = player.y
+    bullet.r = math.atan2(love.mouse.getY() - player.y, love.mouse.getX() - player.x)
+    bullet.speed = 500
+
+    table.insert(bullets, bullet)
+end
+
+function distanceBetweenCoordinates(x1, y1, x2, y2)
+
+    return math.sqrt((x2 - x1)^2 + (y2 - y1)^2)
 end
