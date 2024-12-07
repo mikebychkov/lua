ecb1a8b82d20dfa6844feed4f3abd74242c37b62
mike <mihailbichkov@gmail.com>
2024-12-07 11:37:18 +0500

    platformer init

---
+++ love2d/03-platformer/main.lua
@@ -0,0 +1,19 @@
+function love.load()
+    
+end
+
+function love.update()
+    
+end
+
+function love.draw()
+    
+end
+
+function love.keypressed(key)
+    
+end
+
+function love.mousepressed(x, y, button)
+    
+end
